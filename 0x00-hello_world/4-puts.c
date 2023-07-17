498634fac3c723be578f4625809a9c8ce0d39f04
ingchrist <cdjinguet@gmail.com>
2023-07-17 11:20:03 -0700

    added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-/*short simple code */
-int main() {
-  puts("Programming is like building a multilingual puzzle");
-  return 0;
+/**
+ *sumerisation: main - Prints out some text  string to stdout.
+ * Return: 0 on validation.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
