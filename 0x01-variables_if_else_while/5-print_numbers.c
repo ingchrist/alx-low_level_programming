94c15b36adfdb1935cf56c1c451c6b4c3cf3e043
ingchrist <cdjinguet@gmail.com>
2023-07-17 10:05:12 -0700

    added

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Prints each and every number from 0-9 inclusive
+(*
+ * Return: 0 on validation
+ */
+int main(void)
+{
+	printf("0123456789\n");
+	return (0);
+}
