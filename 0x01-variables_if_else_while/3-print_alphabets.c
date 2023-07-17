da6b0a405f1c77465112e82335a005463991a7a0
ingchrist <cdjinguet@gmail.com>
2023-07-17 09:22:41 -0700

    added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Prints  alphabet in lowercase,and also uppercase.
+(*
+ * Return: 0 on validation
+ */
+int main(void)
+{
+	char c = 'a';
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	putchar('\n');
+	return (0);
+
+}
