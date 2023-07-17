4daa12db5f6476c1c580ef5cbe8275dc83e53c40
ingchrist <cdjinguet@gmail.com>
2023-07-17 11:26:09 -0700

    added

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * description: main - Prints some text  string to stdout.
+ * Return: 0 on validation
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
