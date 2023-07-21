48997c238499662f19a2bdb1a3f182aef4d3b099
ingchrist <cdjinguet@gmail.com>
2023-07-20 19:42:00 -0700

    added

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+/**
+ * _isdigit - check for all digits 1-9
+ * @c: character to check.
+ * Return: 1 if c is a digit, else return 0.
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else
+		return(0);
+}
