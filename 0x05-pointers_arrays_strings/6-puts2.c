779a793c7af89c6affa8bd58de103102604fd957
ingchrist <cdjinguet@gmail.com>
2023-07-24 16:23:31 -0700

    added

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - print every other characters then a new line.
+ * @str: string to print
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (!(i % 2) || i == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
+
