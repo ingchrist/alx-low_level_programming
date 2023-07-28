bfd960c3d37d6513dda8d081f90785b8b192cba7
ingchrist <cdjinguet@gmail.com>
2023-07-28 15:57:41 -0700

    added

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int wz;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	wz = 0;
+	while (s1[wz] != '\0' && s2[wz] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[wz] != s2[wz])
 		{
-			return (s1[i] - s2[i]);
+			return (s1[wz] - s2[wz]);
 		}
-	i++;
+	wz++;
 	}
 	return (0);
 }
