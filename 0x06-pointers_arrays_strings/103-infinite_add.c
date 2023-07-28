a060197faeada47369f061a686c7def238e668dd
ingchrist <cdjinguet@gmail.com>
2023-07-28 16:39:50 -0700

    added

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * rev_string - reverse array
- * @n: integer params
+ * rev_string - revers array
+ * @n: intege param
  * Return: 0
  */
 
 void rev_string(char *n)
 {
 	int i = 0;
-	int j = 0;
+	int wz = 0;
 	char temp;
 
 	while (*(n + i) != '\0')
@@ -18,10 +18,10 @@ void rev_string(char *n)
 	}
 	i--;
 
-	for (j = 0; j < i; j++, i--)
+	for (wz = 0; wz < i; wz++, i--)
 	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
+		temp = *(n + wz);
+		*(n + wz) = *(n + i);
 		*(n + i) = temp;
 	}
 }
@@ -37,27 +37,27 @@ void rev_string(char *n)
 
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int overflow = 0, i = 0, j = 0, digits = 0;
+	int overflow = 0, i = 0, wz = 0, digits = 0;
 	int val1 = 0, val2 = 0, temp_tot = 0;
 
 	while (*(n1 + i) != '\0')
 		i++;
-	while (*(n2 + j) != '\0')
-		j++;
+	while (*(n2 + wz) != '\0')
+		wz++;
 	i--;
-	j--;
-	if (j >= size_r || i >= size_r)
+	wz--;
+	if (wz >= size_r || i >= size_r)
 		return (0);
-	while (j >= 0 || i >= 0 || overflow == 1)
+	while (wz >= 0 || i >= 0 || overflow == 1)
 	{
 		if (i < 0)
 			val1 = 0;
 		else
 			val1 = *(n1 + i) - '0';
-		if (j < 0)
+		if (wz < 0)
 			val2 = 0;
 		else
-			val2 = *(n2 + j) - '0';
+			val2 = *(n2 + wz) - '0';
 		temp_tot = val1 + val2 + overflow;
 		if (temp_tot >= 10)
 			overflow = 1;
@@ -67,7 +67,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			return (0);
 		*(r + digits) = (temp_tot % 10) + '0';
 		digits++;
-		j--;
+		wz--;
 		i--;
 	}
 	if (digits == size_r)
