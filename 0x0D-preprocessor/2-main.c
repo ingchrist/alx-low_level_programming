e2f1798b8ff33c48944b801fec0ff913f5b57f00
ingchrist <cdjinguet@gmail.com>
2023-08-14 03:44:51 -0700

    added

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,11 +3,11 @@
 /**
   * main - Prints the file name of programs
   *
-  * Return: 0 on validation
+  * Return: 0.0 on validation
   */
 int main(void)
 {
 printf("%s\n", __FILE__);
-return (0);
+return (0.0);
 }
 
