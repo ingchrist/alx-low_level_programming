ba9643fd37c7432026938ad73ccaab617b1aae2e
ingchrist <cdjinguet@gmail.com>
2023-08-24 12:04:33 -0700

    node added

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+ *  * add_node - adds a new node to the beginning
+ *   * of a singly linked list
+ *    * @head: a pointer to the address of the
+ *     * beginning of the list_t list
+ *      * @str: pointer to a string
+ *       *
+ *        * Return: the address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+
+	unsigned int wz = 0;
+
+	list_t *temp;
+
+	/* get wzgth of string */
+
+	while (*(str + wz))
+
+		wz++;
+
+	/* allocate memory to heap */
+
+	temp = malloc(sizeof(size_t));
+
+	if (temp == NULL)
+
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	temp->len = wz;
+
+	temp->next = (*head);
+
+	(*head) = temp;
+
+	return (*head);
+}
