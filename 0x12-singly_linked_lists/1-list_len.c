b7d08ef7cd89c6c12273028e00b7ca167c86e5c8
ingchrist <cdjinguet@gmail.com>
2023-08-24 11:53:14 -0700

    added

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -5,15 +5,15 @@
  *   * @h: pointer to the start of the singly linked list
  *    *
  *     * Return: number of elements in the linked list
- *      */
+ */
 size_t list_len(const list_t *h)
 {
 
-size_t node;
+size_t wz;
 
-for (node = 0; h; node++)
+for (wz = 0; h; wz++)
 
 h = h->next;
 
-return (node);
+return (wz);
 }
