8c247332823e5d250e94164b9eef145801d2e990
ingchrist <cdjinguet@gmail.com>
2023-08-24 12:45:40 -0700

    100first added

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void fwz(void) __attribute__ ((constructor));
+
+/**
+ *  * first - print something before main
+ *   *
+ *    * Return: void
+ */
+void fwz(void)
+{
+printf("You're beat! and yet, you must allow,\n"
+
+"I bore my house upon my back!\n");
+}
