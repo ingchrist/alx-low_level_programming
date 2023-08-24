1666627dc8530b5e8deaa1c92531bb23b7efc639
ingchrist <cdjinguet@gmail.com>
2023-08-24 11:44:45 -0700

    list 2 added

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *  * list_len - counts the number of elements in a linked list
+ *   * @h: pointer to the start of the singly linked list
+ *    *
+ *     * Return: number of elements in the linked list
+ *      */
+size_t list_len(const list_t *h)
+{
+
+size_t wz;
+
+for (wz = 0; h; wz++)
+
+h = h->next;
+
+return (wz);
+}
