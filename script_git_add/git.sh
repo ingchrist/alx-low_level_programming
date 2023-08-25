879eeb72408784359baa27130b93b9062274aa9d
ingchrist <cdjinguet@gmail.com>
2023-08-24 17:06:35 -0700

    script updated

--- script_git_add/git.sh
+++ script_git_add/git.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+# Get the commit message from the user
+read -p "Enter a commit message: " commit_message
+
 # Add all the files in the current directory to the staging area
 git add .
 
 # Commit the changes to the local repository
-git commit -m "Your commit message"
+git commit -m "$commit_message"
 
 # Push the changes to the remote repository
 git push origin master
