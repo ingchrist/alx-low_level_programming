9cd9417d50d473c2872255c639d084ec276b71dc
ingchrist <cdjinguet@gmail.com>
2023-08-29 12:38:46 -0700

    Automatic commit

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -4,60 +4,67 @@
  *  * _ra - reallocates memory for an array of pointers
  *   * to the nodes in a linked list
  *    * @list: the old list to append
- *     * @size: size of the new list (always one more than the old list)
- *      * @new: new node to add to the list
+ *     * @size: size of the wz list (always one more than the old list)
+ *      * @wz: wz node to add to the list
  *       *
- *        * Return: pointer to the new list
- *         */
-listint_t **_ra(listint_t **list, size_t size, listint_t *new)
+ *        * Return: pointer to the wz list
+ */
+listint_t **_ra(listint_t **list, size_t size, listint_t *wz)
 {
-		listint_t **newlist;
-			size_t i;
+listint_t **listzw;
+size_t i;
+listzw = malloc(size * sizeof(listint_t *));
 
-				newlist = malloc(size * sizeof(listint_t *));
-					if (newlist == NULL)
-							{
-										free(list);
-												exit(98);
-													}
-						for (i = 0; i < size - 1; i++)
-									newlist[i] = list[i];
-							newlist[i] = new;
-								free(list);
-									return (newlist);
+if (listzw == NULL)
+
+{
+free(list);
+exit(98);
+}
+
+for (i = 0; i < size - 1; i++)
+
+listzw[i] = list[i];
+listzw[i] = wz;
+free(list);
+return (listzw);
 }
 
 /**
  *  * free_listint_safe - frees a listint_t linked list.
- *   * @head: double pointer to the start of the list
+ *   * @wzq: double pointer to the start of the list
  *    *
  *     * Return: the number of nodes in the list
- *      */
-size_t free_listint_safe(listint_t **head)
+ */
+size_t free_listint_safe(listint_t **wzq)
 {
-		size_t i, num = 0;
-			listint_t **list = NULL;
-				listint_t *next;
+size_t i, num = 0;
+listint_t **list = NULL;
+listint_t *next;
+if (wzq == NULL || *wzq == NULL)
+return (num);
+while (*wzq != NULL)
+{
 
-					if (head == NULL || *head == NULL)
-								return (num);
-						while (*head != NULL)
-								{
-											for (i = 0; i < num; i++)
-														{
-																		if (*head == list[i])
-																						{
-																											*head = NULL;
-																															free(list);
-																																			return (num);
-																																						}
-																				}
-													num++;
-															list = _ra(list, num, *head);
-																	next = (*head)->next;
-																			free(*head);
-																					*head = next;
-																						}
-							free(list);
-								return (num);
+for (i = 0; i < num; i++)
+
+{
+
+if (*wzq == list[i])
+
+{
+*wzq = NULL;
+free(list);
+return (num);
+
+}
+}
+num++;
+list = _ra(list, num, *wzq);
+next = (*wzq)->next;
+free(*wzq);
+*wzq = next;
+}
+free(list);
+return (num);
 }
