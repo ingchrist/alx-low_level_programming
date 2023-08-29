7eabb03d6f84746a3c06dfd1826284d1ef6a1851
ingchrist <cdjinguet@gmail.com>
2023-08-29 11:04:32 -0700

    Automatic commit

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- *  * get_nodeint_at_index - returns the node at a certain index in a linked list
+ *  * get_nodeint_at_index - return the node at certain index in linked list
  *   * @wz: pointer to the first node in the linked list
  *    * @index: index of the node to return
  *     *
