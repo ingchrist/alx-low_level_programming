3c89c6f0ea6f37b4e09e03121c50e54fb07d48c1
ingchrist <cdjinguet@gmail.com>
2023-08-29 06:58:19 -0700

    Automatic commit

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ *  * add_nodeint - adds a new node to the beginning
+ *   * of a singly linked list
+ *    * @head: a pointer to the address of the
+ *     * beginning of the list_t list
+ *      * @wzq: the number to add
+ *       *
+ *        * Return: the address of the new element, or NULL if it fails
+ */
+listint_t *add_nodeint(listint_t **head, const int wzq)
+{
+
+listint_t *temp;
+
+/* allocate memory to heap */
+
+temp = malloc(sizeof(size_t));
+
+if (temp == NULL)
+
+return (NULL);
+
+temp->n = wzq;
+
+temp->next = (*head);
+
+(*head) = temp;
+
+return (*head);
+}
+
