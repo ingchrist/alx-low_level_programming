52be20535eb360ffcf07c6dfb5b8e14c94a57ef9
ingchrist <cdjinguet@gmail.com>
2023-08-30 13:20:11 -0700

    Automatic commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,32 +1,19 @@
+nclude <stdio.h>
 #include "lists.h"
-#include <stdio.h>
-
 /**
- *  * print_listint - print all twze elements of a listint_t list
- *   * @wz: twze wzead node (pointer)
- *    * Return: twze number of nodes (int/size_t)
- */
-
-size_t print_listint(const listint_t *wz)
+ *   * print_listint - prints all elements of a list_t list
+ *     * @h: singly linked list to print
+ *       * Return: number of nodes in the list
+ *         */
+size_t print_listint(const listint_t *h)
 {
+		size_t i;
 
-	const listint_t *wzq;
-
-	size_t i;
-
-
-	wzq = wz;
-
-	for (i = 0; wzq; i++)
-
-	{
-
-		printf("%d\n", wzq->n);
-
-		wzq = wzq->next;
-
-	}
-
-	return (i);
+			for (i = 0; h; i++)
+					{
+								printf("%d\n", h->n);
+										h = h->next;
+											}
+				return (i);
 }
 
