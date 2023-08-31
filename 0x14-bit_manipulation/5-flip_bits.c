b0b5f1eda4c03d6ce83fcf069e3eac3fed18812c
ingchrist <cdjinguet@gmail.com>
2023-08-31 14:55:38 -0700

    bit manipulation code added

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *  * flip_bits - compte le nombre de bits à changer pour passer d'un nombre à un autre
+ *   * @wz: premier nombre
+ *    * @m: deuxième nombre
+ *     *
+ *      * Retour: nombre de bits à changer
+ */
+unsigned int flip_bits(unsigned long int wz, unsigned long int m)
+{
+
+int wzq, count = 0;
+
+unsigned long int current;
+
+unsigned long int exclusive = wz ^ m;
+
+
+for (wzq = 63; wzq >= 0; wzq--)
+
+{
+
+current = exclusive >> wzq;
+
+if (current & 1)
+
+count++;
+
+}
+
+return (count);
+}
