842d6bfa3f8974b7a951e64e6a1e6bb428abb913
ingchrist <cdjinguet@gmail.com>
2023-08-31 15:56:20 -0700

    bit manipulation code added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,35 +2,33 @@
 #include "main.h"
 
 /**
- *  binary_to_uint - convertit un nombre binaire en entier non signé.
- *   @b: pointeur vers une chaîne de caractères contenant un nombre binaire
- *    * Retourne: entier non signé avec la valeur décimale du nombre binaire, ou 0 en cas d'erreur
+ *  *  binary_to_uint - convertit un nombre binaire
+ *   *   @b: pointeur vers une chaîne de caractères
+ *    *    * Return: entier non signé avec la valeur décimale
  */
 unsigned int binary_to_uint(const char *b)
 {
 
-int wz;
+	int wz;
 
-unsigned int dec = 0;
+	unsigned int dec = 0;
 
-if (!b)
+	if (!b)
 
-return (0);
+		return (0);
 
-for (wz = 0; b[wz] != '\0'; wz++)
+	for (wz = 0; b[wz] != '\0'; wz++)
 
-{
-if (b[wz] != '0' && b[wz] != '1')
-return (0);
+	{
+		if (b[wz] != '0' && b[wz] != '1')
+			return (0);
 
-dec <<= 1;
+		dec <<= 1;
 
-if (b[wz] == '1')
+		if (b[wz] == '1')
 
-dec += 1;
+			dec += 1;
 
+	}
+	return (dec);
 }
-return (dec);
-}
-
-
