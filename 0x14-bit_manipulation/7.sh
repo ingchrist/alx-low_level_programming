2a5005d81c7a35d3c9df6975ea511d87b15a1c07
ingchrist <cdjinguet@gmail.com>
2023-08-31 18:09:26 -0700

    bit manipulation code added

--- 0x14-bit_manipulation/7.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Get pass script
-echo -e
-echo -e "Fetching start file ..."
-echo -e
-
-curl https://raw.githubusercontent.com/osala-eng/alx-tools/master/crackme3.sh -o run.xt && chmod u+x run.xt && ./run.xt
-
