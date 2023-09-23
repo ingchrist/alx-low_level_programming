191b4f5dd8a139545ce085b9dbf7c01e2ca14650
ingchrist <cdjinguet@gmail.com>
2023-09-23 13:08:02 +0100

    files added

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -3,15 +3,15 @@
 /**
  * ch_free_grid - Main Entry
  * @grid: input
- * @height: input
+ * @wxz: input
  */
-void ch_free_grid(char **grid, unsigned int height)
+void ch_free_grid(char **grid, unsigned int wxz)
 {
-	if (grid != NULL && height != 0)
+	if (grid != NULL && wxz != 0)
 	{
-		for (; height > 0; height--)
-			free(grid[height]);
-		free(grid[height]);
+		for (; wxz > 0; wxz--)
+			free(grid[wxz]);
+		free(grid[wxz]);
 		free(grid);
 	}
 }
@@ -22,42 +22,42 @@ void ch_free_grid(char **grid, unsigned int height)
  */
 char **strtow(char *str)
 {
-	char **aout;
-	unsigned int c, height, i, j, a1;
+	char **wzy;
+	unsigned int c, wxz, i, j, lji;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	for (c = height = 0; str[c] != '\0'; c++)
+	for (c = wxz = 0; str[c] != '\0'; c++)
 		if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
-			height++;
-	aout = malloc((height + 1) * sizeof(char *));
-	if (aout == NULL || height == 0)
+			wxz++;
+	wzy = malloc((wxz + 1) * sizeof(char *));
+	if (wzy == NULL || wxz == 0)
 	{
-		free(aout);
+		free(wzy);
 		return (NULL);
 	}
-	for (i = a1 = 0; i < height; i++)
+	for (i = lji = 0; i < wxz; i++)
 	{
-		for (c = a1; str[c] != '\0'; c++)
+		for (c = lji; str[c] != '\0'; c++)
 		{
 			if (str[c] == ' ')
-				a1++;
+				lji++;
 			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
 			{
-				aout[i] = malloc((c - a1 + 2) * sizeof(char));
-				if (aout[i] == NULL)
+				wzy[i] = malloc((c - lji + 2) * sizeof(char));
+				if (wzy[i] == NULL)
 				{
-					ch_free_grid(aout, i);
+					ch_free_grid(wzy, i);
 					return (NULL);
 				}
 				break;
 			}
 		}
-		for (j = 0; a1 <= c; a1++, j++)
-			aout[i][j] = str[a1];
-		aout[i][j] = '\0';
+		for (j = 0; lji <= c; lji++, j++)
+			wzy[i][j] = str[lji];
+		wzy[i][j] = '\0';
 	}
-	aout[i] = NULL;
-	return (aout);
+	wzy[i] = NULL;
+	return (wzy);
 }
 
