f5e1c3cd013895e4ebda0dbf76b62edc201ce7fc
ingchrist <cdjinguet@gmail.com>
2023-09-23 19:51:34 +0100

    files added

--- 0x03-debugging/0-positive_or_negative.c
+++ 0x03-debugging/0-positive_or_negative.c
@@ -21,26 +21,3 @@ void positive_or_negative(int i)
 									printf("%d is negative\n", i);
 										}
 }
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
-
-/**
- *  * main - checks weather the generated number positive, zero or negative.
- *  **/
-
-void positive_or_negative(int i)
-{
-		if (i > 0)
-				{
-							printf("%d is positive\n", i);
-								}
-			else if (i == 0)
-					{
-								printf("%d is zero\n", i);
-									}
-				else
-						{
-									printf("%d is negative\n", i);
-										}
-}
