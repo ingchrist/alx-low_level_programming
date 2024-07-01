31892f3d171fc48bc63775401e6fdaa29a2ac418
ingchrist <cdjinguet@gmail.com>
2024-07-01 23:26:44 +0100

    files added

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -23,3 +23,10 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
+                       
+              
+       
+            
+        
+         
+
